bb27b3209c427f72edf94b7ba87cdf943b3f885c
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-05 15:11:35 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -26,7 +26,9 @@ const spectrogramRows = 1024
 // Create charts and series.
 
 // NOTE: Using `Dashboard` is no longer recommended for new applications. Find latest recommendations here: https://lightningchart.com/js-charts/docs/basic-topics/grouping-charts/
-const dashboard = lightningChart()
+const dashboard = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
     .Dashboard({
         theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined
         numberOfColumns: 2,
